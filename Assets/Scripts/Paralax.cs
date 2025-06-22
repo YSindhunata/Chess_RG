3eb86ef29afffe4597b3e21947eda2c918b126dd
Hyadawild <kudosinichi9290@gmail.com>
2025-06-22 13:15:08 +0700

    homeewek

---
+++ Assets/Scripts/Paralax.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class Paralax : MonoBehaviour
+{
+    public float offsetMultiplier = 1f;
+    public float smoothTime = .3f;
+
+    private Vector2 startPosition;
+    private Vector3 velocity;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        startPosition = transform.position;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 offset = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+    }
+}
